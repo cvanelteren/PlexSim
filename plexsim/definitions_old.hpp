c55da6bc74e35b99930d6b305891921082b97f79
Casper van Elteren <caspervanelteren@gmail.com>
2020-05-01 11:17:32 +0200

    cpp still slower than cython
    Former-commit-id: e40276da2695d02627e2f93fe4d8f94ca22f9018

--- plexsim/definitions_old.hpp
+++ plexsim/definitions_old.hpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <math.h>
 #include <pybind11/stl.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
-#include "randutils.hpp"
 #define FORCE_IMPORT_ARRAY
 #include "xtensor/xrandom.hpp"
 #include "xtensor-python/pyarray.hpp"
-#include "xtensor/xmath.hpp"
-#include "xtensor/xio.hpp"
+#include "xtensor-python/pycontainer.hpp"
 
+#include <random>
 #include <ctime>
 namespace py = pybind11;
 using namespace pybind11::literals;
@@ -25,19 +23,12 @@ typedef xt::xarray<nodeState_t> nodeStates;
 typedef std::vector<long> agentStates_t;
 
 // sampling binding
-typedef xt::xarray<nodeID_t> nodeids_a;
-
-typedef  xt::xarray<nodeID_t> Nodeids;
+typedef xt::xarray<nodeID_t> samples_t;
 // typedef std::array<nodeID_t> samples_t;
 
 // Adjacency definition
 typedef unordered_map<nodeID_t, weight_t> Neighbors;
 struct Connection{
-  Neighbors neighbors;
+    Neighbors neighbors;
 };
-
-
 typedef unordered_map<nodeID_t, Connection> Connections;
-
-
-
