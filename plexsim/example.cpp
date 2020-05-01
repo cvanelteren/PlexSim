e40276da2695d02627e2f93fe4d8f94ca22f9018
Casper van Elteren <caspervanelteren@gmail.com>
2020-05-01 11:17:32 +0200

    cpp still slower than cython

--- plexsim/example.cpp
+++ plexsim/example.cpp
@@ -40,16 +40,18 @@ public:
       }
   };
 
-  Connections adj; // adjecency
-  py::object graph; // stash nx object inside the class
+    Connections adj; // adjecency
+    py::object graph; // stash nx object inside the class
 
-  nudgeProperty nudgeType;
-  updateProperty updateType;
+    nudgeProperty nudgeType;
+    updateProperty updateType;
 
-  int sampleSize;
-  int nNodes;
+    unsigned int sampleSize;
+    unsigned int nNodes;
 
-  samples_t samples;
+    unsigned int nStates;
+    randutils::mt19937_rng rng;
+  
   xt::xarray<nodeID_t> nodeids;
 
   // constructor
@@ -61,29 +63,29 @@ public:
         int seed               = -1\
         ) {
 
-    cout << "In base constuctor\n";
-    // setup seed
-    std::time_t ts = std::time(nullptr);
-    if (seed < 0 ){
-      seed = ts;
-    }
-    this->_dist = std::uniform_real_distribution<double>(0., 1.);
-    this->_gen  = std::mt19937(seed);
+      // setup seed
+      // std::time_t ts = std::time(nullptr);
+      // if (seed < 0 ){
+      //    this->_gen  = std::mt19937_64(0);
+      //}
+      //else {
+      //    this -> _gen = std::mt19937_64(seed);
+      //}
+      //this->_dist = std::uniform_real_distribution<double>(0., 1.);
+      // create adj
+      create_adj(graph);
 
-
-    // create adj
-    create_adj(graph);
-
-    // setup properties
-    this->sampleSize = sampleSize;
-    this->nudgeType  = nudgeType;
-    this->updateType = updateType;
+      // setup properties
+      this->sampleSize = sampleSize;
+      this->nudgeType  = nudgeType;
+      this->updateType = updateType;
+    this->nStates    = agentStates.size();
 
     if (sampleSize <= 0) this->sampleSize = this->nNodes;
 
     // setup buffers
-    this->states     = nodeStates(this->nNodes);
-    this->newstates  = nodeStates(this->nNodes);
+    this->states     = nodeStates({this->nNodes} );
+    this->newstates  = nodeStates({this->nNodes});
   }
   virtual ~Model()  = default;
 
@@ -114,67 +116,117 @@ public:
 
        // add target to source only
        if (directed){
-         this->adj[target].neighbors[source] = weight;
+           this->adj[target].neighbors[source] = weight;
        }
        else{
-         this->adj[source].neighbors[target] = weight;
-         this->adj[target].neighbors[source] = weight;
+           this->adj[source].neighbors[target] = weight;
+           this->adj[target].neighbors[source] = weight;
        };
     };
     // hide model
     this-> graph = graph;
-    this-> nNodes= this->adj.size();
+    this->nNodes = static_cast <unsigned int>(py::int_(graph.attr("number_of_nodes")()));
 
-    this->nodeids = xt::xarray<nodeID_t>(this->nNodes);
-    for (auto node = 0; node < nNodes; ++node){
-        this->nodeids[node] = node;
-    }
-
+    this->nodeids = xt::arange(this->nNodes);
     return ;
   }
-  double rand(){
-    return this->_dist(this->_gen);
-  }
+    //double rand(){
+    //  return this->_dist(this->_gen);
+    //}
 
-    samples_t sampleNodes(int nSamples){
+    nodeids_a sampleNodes(uint nSamples){
 
-    int sampleSize   = this->sampleSize;
-    int nNodes       = this->nNodes;
+       unsigned int sampleSize   = this->sampleSize;
+       int nNodes       = this->nNodes;
+       auto nodeids     = this->nodeids;
+       // samples samples = this->samples;
+       // samples.resize(nSamples * sampleSize);
+       unsigned int N   = nSamples * sampleSize;
+       nodeids_a nodes  = nodeids_a::from_shape({N});
 
-    auto nodeids = this->nodeids;
-    // samples_t samples = this->samples;
-    // samples.resize(nSamples * sampleSize);
-    int N = nSamples * sampleSize;
-    samples_t samples = samples_t::from_shape({N});
-    for (int samplei = 0; samplei < N; ++samplei){
-      // shuffle the node ids
-      // start = (samplei * sampleSize) % nNodes;
-      if (samplei % nNodes >= nNodes){
-          xt::random::shuffle(nodeids);
-          }
-      // assign to sample
-      //ptr[samplei] = nodeids[samplei % nNodes];
-      samples[samplei] = nodeids[samplei % nNodes];
+       int idx;
+       for (uint samplei = 0; samplei < N; samplei++){
+         // shuffle the node ids
+         // start = (samplei * sampleSize) % nNodes;
+           if (!(samplei % nNodes)){
+               this->rng.shuffle(nodeids);
+         }
+         // assign to sample
+         //ptr[samplei] = nodeids[samplei % nNodes];
+           nodes[samplei] = nodeids[samplei % nNodes];
     }
-    return samples;
+    return xt::reshape_view(nodes, {nSamples, sampleSize});
   }
-  // Implement per model
-  virtual void step(nodeID_t node_id)  = 0;
 
-  virtual void swap_buffers(){
-    std::swap(this->states, this->newstates);
-  }
+    // Implement per model
+  nodeStates updateState(nodeids_a nodes){
+        /*
+          Node update loop
+        */
+      // for (auto node = 0; node < this->nNodes; node++){
+      //     this->step(nodes[node]);
+      // }
+      for (auto node : nodes){
+          this->step(node);
+      }
+        this->swap_buffers();
+        return this->states;
+    }
+    //implement inherited class
+    virtual void step(nodeID_t node_id)  = 0;
+
+    virtual void swap_buffers(){
+        std::swap(this->states, this->newstates);
+     }
+
+    nodeStates  simulate(unsigned int nSamples){
+
+        nodeStates results = nodeStates::from_shape({nSamples, this->sampleSize});
+
+        for (uint samplei = 0 ; samplei < nSamples; samplei++){
+            xt::view(results, samplei) = this->updateState(this->sampleNodes(1));
+            }
+        return results;
+    }
 
 private:
-  std::mt19937 _gen; // RNG generator 
-  std::uniform_real_distribution<double> _dist; 
+  // std::mt19937_64 _gen; // RNG generator 
+  // std::uniform_real_distribution<double> _dist; 
+  // randutils::mt199337_rng rng;
 
+    
   // buffers to write updates to
   nodeStates states;
   nodeStates newstates;
 };
 
+class Potts: public Model{
+public:
+    Potts(\
+     py::object graph,                               \
+     agentStates_t  agentStates = agentStates_t(1, 0), \
+     string nudgeType       = "constant",\
+     string updateType      = "async",\
+     int sampleSize         = 0, \
+     int seed               = -1\
+          ): Model(graph, agentStates,\
+                   nudgeType, updateType,\
+                   sampleSize, seed){
+        
+    };
 
+   void step(nodeID_t node){
+       xt::xarray<double> energies = xt::xarray<double>({3});
+
+       
+       return ;
+    }
+
+    double hamiltonian(nodeID_t x, nodeID_t y){
+        double delta = 2. * xt::numeric_constants<double>::PI * (x - y) / double(this->nStates); 
+        return cos(delta);
+    }
+};
 
 
 
@@ -194,7 +246,20 @@ public:
                             step, //function name
                             node_id//arguments
                             );
-   }
+     }
+};
+
+
+template <class Base = Potts>
+class PyPotts: public PyModel<Base>{
+public:
+    using PyModel<Base>::PyModel;
+
+    void step (nodeID_t node) override{
+        PYBIND11_OVERLOAD(void, Base, step, node); 
+    }
+
+
 };
 
 
@@ -216,11 +281,30 @@ PYBIND11_MODULE(example, m){
          "seed"_a        = -1
          )
     .def_readwrite("graph", &Model::graph)
-      .def("sampleNodes", &Model::sampleNodes,
+    .def("sampleNodes", &Model::sampleNodes,
            "Class method for sampling nodes")
+    .def("simulate", &Model::simulate)
       ;//end class definition
 
+  py::class_<Potts, PyPotts<>>(m, "Potts")
+      .def(py::init<\
+           py::object, \
+           agentStates_t,\
+           string,\
+           string,\
+           int,\
+           int        >(),
+           "graph"_a       ,
+           "agentStates"_a = agentStates_t({0, 1}),
+           "nudgeType"_a   = "constant",
+           "updateType"_a  = "async",
+           "sampleSize"_a  = -1,
+           "seed"_a        = -1
+           )
+      .def_readwrite("graph", &Potts::graph)
+      .def("sampleNodes", &Potts::sampleNodes,
+           "Class method for sampling nodes")
+      .def("simulate", &Potts::simulate)
+      ;
   m.doc() = "Testing this stuff out";
 }
-
-
