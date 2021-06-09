71c17ffcddb058ee1390c0bcee0a5ea6b11da51e
cvanelteren <caspervanelteren@gmail.com>
2021-06-09 16:52:44 +0200

    updated docstr for get_include

--- plexsim/__init__.py
+++ plexsim/__init__.py
@@ -1,23 +1,23 @@
 import os
 
 
+# this function is inspired by numpy
 def get_include():
     """
-    Return the directory that contains the NumPy \\*.h header files.
-    Extension modules that need to compile against NumPy should use this
+    Return the directory that contains the plexim \\*.h header files.
+    Extension modules that need to compile against plexsim should use this
     function to locate the appropriate include directory.
     Notes
     -----
     When using ``distutils``, for example in ``setup.py``.
     ::
-        import numpy as np
+        import plexsim
         ...
         Extension('extension_name', ...
-                include_dirs=[np.get_include()])
+                include_dirs=[plexsim.get_include()])
         ...
     """
     import plexsim
 
-    # d = os.path.join(os.path.dirname(__file__), "include")
     d = os.path.join(os.path.dirname(__file__), "../")
     return d
