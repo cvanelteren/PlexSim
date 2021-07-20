1ff6bb74cc5c058763ecf5886879198b896768a1
cvanelteren <caspervanelteren@gmail.com>
2021-07-20 07:14:17 +0200

    pushed backup to c++ branch

--- BACK_PLEX/weird.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <math.h>
-int main(){
-  size_t d = 1;
-  std::cout<< "whatever" << d;
-}
--- BACK_PLEX/weird.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx, numpy as np
-from example import PD
-
-a = np.arange(3, dtype = np.float64)
-g = nx.grid_graph((3,3))
-m = PD(g, t = 1.5,
-       sampleSize = 3)
-b = m.simulate(10)
-print(b)
